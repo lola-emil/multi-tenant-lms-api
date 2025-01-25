9797355d8e279f2c6bb7e5bca02efe00850c3f2b
Rens Salaladaga <staleexam19@gmail.com>
2025-01-25 08:42:33 +0800

    feat:  feature

--- src/utils/crudrepo.ts
+++ src/utils/crudrepo.ts
@@ -2,53 +2,69 @@ import { db } from "../config/database";
 import { ErrorResponse } from "../middlewares/errorhandler";
 
 
-type PossibleQuery<T> = {
+type QueryModifiers<T> = {
     offset?: number,
     limit?: number,
     cols?: string,
 
+    order?: "desc" | "asc",
+
+    orderby?: keyof T 
 
 } & T;
 
 export default class CrudRepo<T> {
-
     private tableName: string;
-
-
-    private parseURLQuery(query: PossibleQuery<Partial<T>>) {
-        return query;
-    }
-
-    private omitHelperQueries(query: PossibleQuery<Partial<T>>) {
-        delete query.limit;
-        delete query.offset;
-        delete query.cols;
-
-        return query;
-    }
 
     constructor(tableName: string) {
         this.tableName = tableName;
     }
 
-    async find(query: PossibleQuery<Partial<T>>) {
+
+    private omitQueryModifiers(query: QueryModifiers<Partial<T>>) {
+        const { limit, offset, cols, order, orderby, ...filteredQuery } = query;
+        return filteredQuery;
+    }
+
+    private async getTableCols(): Promise<string[]> {
+        const columns = await db(this.tableName).columnInfo();
+        return Object.keys(columns);
+    }
+
+    private async sanitizedCols(cols: string): Promise<string[]> {
+        const allowedCols = await this.getTableCols();
+
+        return cols.split(",").filter(col => allowedCols.includes(col.trim()));
+    }
+
+    async find(query: QueryModifiers<Partial<T>>) {
         try {
             const sql = db(this.tableName);
 
-            if (query.cols)
-                sql.select(query.cols.split(","));
+            if (!query.limit || query.limit < 0)
+                query.limit = 10;
 
-            if (query.limit && query.limit > 0)
-                sql.limit(query.limit);
+            if (query.cols) sql.select(await this.sanitizedCols(query.cols));
+
+            // Apply limit and offset
+            sql.limit(query.limit);
 
             if (query.offset)
                 sql.offset(query.offset);
+            
+
+            // TODO: validation para sa orderby, kay mag error siya kung dili
+            // mao ang column name
+            if (query.order || query.orderby)
+                sql.orderBy(query.orderby ?? "id", query.order ?? "asc");
 
             // removed the helper queries para ma filter ang columns
-            sql.where(this.omitHelperQueries(query));
+            sql.where(this.omitQueryModifiers(query));
+
             return await sql;
         } catch (error) {
-            throw new ErrorResponse(400, (<any>error).sqlMessage);
+            console.log(error);
+            throw new ErrorResponse(400, "Invalid query");
         }
     }
 
