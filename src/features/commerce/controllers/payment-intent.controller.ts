52c6bfd13b383982d66fc983bfa18077c5ea03f5
Rens Saladaga <staleexam19@gmail.com>
2025-02-07 13:57:39 +0800

    feat: added the required Auth headers for payments

--- src/features/commerce/controllers/payment-intent.controller.ts
+++ src/features/commerce/controllers/payment-intent.controller.ts
@@ -1,15 +1,14 @@
 import { type Request, type Response } from "express";
 import axios from "axios";
-import { PAYMONGO_API_URL } from "../../../config/constants";
-
-
+import { PAYMONGO_API_URL, PAYMONGO_SECRET_KEY } from "../../../config/constants";
 
 export async function createPaymentIntent(req: Request, res: Response) {
     const body = req.body;
     const response = await axios.post(`${PAYMONGO_API_URL}/payment_intents`, body, {
         headers: {
             "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
         }
     });
 
@@ -21,7 +20,8 @@ export async function retrievePaymentIntent(req: Request, res: Response) {
     
     const response = await axios.get(`${PAYMONGO_API_URL}/payment_intents/${id}`, {
         headers: {
-            "Accept": "application/json"
+            "Accept": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
         }
     });
 
@@ -35,7 +35,8 @@ export async function attachPaymentIntent(req: Request, res: Response) {
     const response = await axios.post(`${PAYMONGO_API_URL}/payment_intents/${id}/attach`, body, {
         headers: {
             "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
         }
     });
 
--- src/features/commerce/controllers/payment-method.controller.ts
+++ src/features/commerce/controllers/payment-method.controller.ts
@@ -1,13 +1,14 @@
 import { type Request, type Response } from "express";
 import axios from "axios";
-import { PAYMONGO_API_URL } from "../../../config/constants";
+import { PAYMONGO_API_URL, PAYMONGO_SECRET_KEY } from "../../../config/constants";
 
 export async function createPaymentMethod(req: Request, res: Response) {
     const body = req.body;
     const response = await axios.post(`${PAYMONGO_API_URL}/payment_methods`, body, {
         headers: {
             "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
         }
     });
 
@@ -15,13 +16,23 @@ export async function createPaymentMethod(req: Request, res: Response) {
 }
 
 export async function retrievePossibleMerchants(req: Request, res: Response) {
-    const response = await axios.get(`${PAYMONGO_API_URL}/merchants/capabilities/payment_methods`);
+    const response = await axios.get(`${PAYMONGO_API_URL}/merchants/capabilities/payment_methods`, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
+        }
+    });
     return res.json(response.data); 
 }
 
 export async function retrievePaymentMethod(req: Request, res: Response) {
     const id = req.params.id;
-    const response = await axios.get(`${PAYMONGO_API_URL}/payment_methods/${id}`);
+    const response = await axios.get(`${PAYMONGO_API_URL}/payment_methods/${id}`, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
+        }
+    });
     return res.json(response.data);
 }
 
@@ -32,7 +43,8 @@ export async function updatePaymentMethod(req: Request, res: Response) {
     const response = await axios.put(`${PAYMONGO_API_URL}/payment_methods/${id}`, body, {
         headers: {
             "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + PAYMONGO_SECRET_KEY
         }
     });
 
